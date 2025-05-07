a585b9dedc48fa0ee829d4fc79e509b7a2775d79
Shantel <Shantelgray24@gmail.com>
2025-05-07 15:07:57 -0400

    Adding conditionals and functions to the first functions lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(42 - pickupLocation);
+}
+function distanceFromHqInFeet(feet) {
+  return Math.abs(42 - feet) * 264;
+}
+function distance (start, destination) {
+  return Math.abs(start - destination)
+}
+function distanceTravelledInFeet(start, destination) {
+   const distanceInBlocks = distance(start, destination)
+ return distanceInBlocks * 264;
+}
+function calculatesFarePrice(start, destination) {
+  const travelDistance = distanceTravelledInFeet(start, destination)
+  if (travelDistance <= 400) {  
+    return 0;
+  } else if (travelDistance <= 2000) {
+    return (travelDistance - 400) * 0.02;
+  } else if (travelDistance <= 2500) {
+    return 25;
+  } else { return 'cannot travel that far'
+  }
+};
+  
